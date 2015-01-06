d260413a4967c3b10f76839c6afe4cfcf63433d6
Paul Montoro <pmontoro88@gmail.com>
2015-01-06 12:32:28 -0600

    added new outputs

---
+++ src/edu/austincc/main/App.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.austincc.main;
+
+/**
+ * @author Paul
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		
+		System.out.println("Hello, again!");
+		
+		System.out.println("Goodbye!");
+	}
+
+}
